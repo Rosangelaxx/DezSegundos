e104e20548272f02bddd3ec5ab683843d4ee6ada
Rosangelaxx <tomaziaalcantara5@gmail.com>
2026-08-24 20:49:07 -0300

    Add files via upload
    DezSegundos

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace DezSegundos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando a contagem de 0 a 10...");
+            Console.WriteLine();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Contagem finalizada!");
+        }
+    }
+}
